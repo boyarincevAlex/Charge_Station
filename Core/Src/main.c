8d262f333f9a250449d62f293374206c876ba09c
JORAmashina <oppaidoto@yandex.ru>
2023-05-20 23:45:09 +0500

    last correcting TIM config

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -205,9 +205,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 3;
+  htim2.Init.Prescaler = 0;
   htim2.Init.CounterMode = TIM_COUNTERMODE_CENTERALIGNED1;
-  htim2.Init.Period = 360;
+  htim2.Init.Period = 359;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
